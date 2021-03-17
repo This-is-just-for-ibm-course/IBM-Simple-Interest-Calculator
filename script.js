0e0ed3d887016269aa6b1de8f55795b3615842b3
This-is-just-for-ibm-course <IbmCourseEmail@gmail.com>
2021-03-17 04:07:31 +0000

    everything should work but for some reason, nothing does

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
+
+const compute = (e) => {
+    e.preventDefault();
     p = document.getElementById("principal").value;
-    
+    i = document.getElementById('rate').value;
+    y = document.getElementById('years').value;
+    document.getElementById('result').innerHTML = y * i * r;
+
 }
-        
+const button = document.getElementById('button');
+button.addEventListener('click', compute);
+const range = document.getElementById('rate');
+const interestRateDisplay = document.getElementsByClassName('interest-rate-display')[0];
+range.addEventListener('change', () => interestRateDisplay.innerHTML = range.value);
+
+
+const milk = document.getElementById('milk');
+const silk = document.getElementById('silk');
+milk.addEventListener('change', (e) => silk.value = e.target.value);
